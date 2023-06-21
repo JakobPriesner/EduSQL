2e1f5932c2dd5cf3bbffccc704f010a1ef5d32a6
TimNöth <tim.noeth@student.fhws.de>
2023-06-21 10:38:15 +0200

    added level_two_task_three to validate_task + renamed level_two_task_one to level_two_task_two

--- backend/api/endpoints/levels/logic/validation/strategies/level_two_task_two.py
+++ backend/api/endpoints/levels/logic/validation/strategies/level_two_task_two.py
@@ -7,18 +7,18 @@ from database.models.db_user import DbUser
 from database.postgresql_connection_interface import IPostgresqlConnection
 
 
-class LevelTwoTaskOneValidator(IConcreteValidation):
+class LevelTwoTaskTwoValidator(IConcreteValidation):
     @inject
     def __init__(self, db: IPostgresqlConnection, db_user_handler: DbUserHandler):
         self._db: IPostgresqlConnection = db
         self._db_user_handler: DbUserHandler = db_user_handler
-        self._admin_user: DbUser = self._db_user_handler.get_user_by_username("admin")
+        self._admin_user: DbUser = self._db_user_handler.get_user_by_username("p_braun")
 
     async def handle(self, user_uuid: str) -> LevelValidationResult:
-        return LevelValidationResult(level="2.1",
+        return LevelValidationResult(level="2.2",
                                      is_valid=True,
                                      message="")
 
     @classmethod
     def can_handle(cls, level_number: int, task_number: int) -> bool:
-        return level_number == 2 and task_number == 1
+        return level_number == 2 and task_number == 2
